a73dcc4abf9784af232d30a764b6d5ca116cc4bc
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-03 23:16:34 +0000

    Completed making a function that acts as .map would. Map Function takes in two argument (arr and length) and iterates through the given array, makes a change ( in this case finding the length of each character) and produces a new array as a result.

--- callback-map.js
+++ callback-map.js
@@ -1,2 +1,25 @@
 // Lighthouse Labs - W1D4 - Exercise Callback - Maps //
 
+var lyrics = ["ground",
+"control",
+ "to",
+ "major",
+ "tom"];
+
+
+// function that takes in a numberic value and returns the length
+function emlLength(num) {
+  var length = num.length;
+  return length;
+}
+
+
+function map(arr, length) {
+  var result = [];
+  for (var i = 0; i < arr.length; i += 1) { // loop through array
+      result.push(emlLength(arr[i])); // add value into array
+  }
+  return result;
+};
+
+console.log(map(lyrics, emlLength));
